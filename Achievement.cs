03bdf6d395d8cb498a53e9adeb5e40743058f92f
monudevdil0301-ux <monudevdil0301@gmail.com>
2026-08-01 18:28:20 +0530

    Add files via upload

---
+++ Achievement.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[System.Serializable]
+public class Achievement
+{
+    public string title;
+
+    public int coinReward;
+    public int gemReward;
+
+    public bool completed;
+    public bool claimed;
+}
